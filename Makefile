57897ec702a264a95a675839af984ecf79fff3eb
Augusto Rocha Carneiro Napoleão <augusto@augustorcn.com.br>
2012-02-11 01:58:47 -0200

    Travis again

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 language: php
 php:
-	5.3
-	5.4
+  - 5.3
+  - 5.4
 before_script:
-	wget -nc http://getcomposer.org/composer.phar
-	php composer.phar install
-	php bin/run_phar
+  - wget -nc http://getcomposer.org/composer.phar
+  - php php composer.phar install
+  - php bin/run_phar
 script:
-	@cd tests;phpunit ./
+  - cd tests
+  - phpunit ./
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,17 +1,8 @@
 <?php
+require '../vendor/.composer/autoload.php';
 
-set_include_path(realpath(dirname(__FILE__) . '/../src') . PATH_SEPARATOR . realpath(dirname(__FILE__) . '../src') . PATH_SEPARATOR . realpath(dirname(__FILE__) . '/../') . PATH_SEPARATOR . get_include_path()
-);
+use Symfony\Component\ClassLoader\UniversalClassLoader;
 
-spl_autoload_register(function($className){		
-	$className = explode('\\', ltrim($className, '\\'));
-	
-	if (true == strpos(end($className), '_'))
-		array_splice($className, -1, 1, explode('_', current($className)));
-
-	$className = implode(DIRECTORY_SEPARATOR, $className) . '.php';	
-	foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
-		if (file_exists($path . DIRECTORY_SEPARATOR . $className))
-			require $path . DIRECTORY_SEPARATOR . $className;	
-	}	
-});
+$loader = new UniversalClassLoader();
+$loader->registerNamespace('Judi', '../src');
+$loader->register();
