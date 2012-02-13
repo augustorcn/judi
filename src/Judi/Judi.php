99ea720afe85af68565b31a76da67095b1eca788
Augusto Rocha Carneiro Napoleão <augusto@augustorcn.com.br>
2012-02-13 01:29:17 -0200

    Refactoring to separate the data passed by yaml configuration file and creating the class Explode to help in this matter.

--- src/Judi/Judi.php
+++ src/Judi/Judi.php
@@ -13,70 +13,13 @@ class Judi
 	
 	public function __construct($config)
 	{
-		$proceduresData = $this->explodeProcedures(Yaml::parse($config));
+		$explode = new Explode(Yaml::parse($config));
+		$this->setCommand($explode->commands());
+		$this->setClass($explode->configuration()->classes());
+		$this->setOptions($explode->configuration()->options());
+		$this->setParams($explode->configuration()->params());
 	}
-	
-	private function explodeProcedures($yamlToArray)
-	{
-		if (!array_key_exists('Commands', $yamlToArray))
-			throw new \Exception("You need to set the key 'Commands'.");
-		foreach ($yamlToArray as $commands) {
-			if (!isset($commands))
-				throw new \Exception('You need to set a command.');
-			$this->explodeCommands($commands);
-		}
-	}
-	
-	private function explodeCommands($commands)
-	{
-		foreach ($commands as $command => $data) {
-			if (!isset($data))
-				throw new \Exception('You need to set a command configuration.');
-			if (!array_key_exists('class', $data))
-				throw new \Exception('You need to set the command class.');
-			
-			$this->setCommand($command);
-			$this->explodeData($data);
-		}
-	}
-	
-	private function explodeData($data)
-	{
-		foreach ($data as $key => $value) {
-			if ($key == 'class') {
-				if (!isset($value))
-					throw new \Exception('You need to set a valid class name.');
-				$this->setClass($value);
-			}
-			if ($key == 'options') {
-				$this->explodeOptions($value);
-			}
-		}
-	}
-	
-	private function explodeOptions($options)
-	{
-		foreach ($options as $option => $params) {
-			$this->setOptions($option);	
-			if (is_array($params)) {
-				if (array_key_exists('params', $params))
-					$this->explodeParams($params);
-			} else {
-				$this->setParams(null);
-			}		
-		}
-	}
-	
-	private function explodeParams($params)
-	{
-		foreach ($params as $param)
-			foreach ($param as $name => $type) {
-				if (!isset($type))
-					throw new \Exception("You need to set a type to param {$name}");
-				$this->setParams($name);
-			}
-	}
-	
+
 	public function run()
 	{
 	}
@@ -88,17 +31,17 @@ class Judi
 	
 	public function setClass($class)
 	{
-		$this->class[$this->command][] = $class;
+		$this->class = $class;
 	}
 	
 	public function setOptions($oprions = null)
 	{
-		$this->options[$this->command][] = $oprions;
+		$this->options = $oprions;
 	}
 	
 	public function setParams($params = null)
 	{
-		$this->params[$this->command][] = $params;
+		$this->params = $params;
 	}
 	
 	public function getCommand()
